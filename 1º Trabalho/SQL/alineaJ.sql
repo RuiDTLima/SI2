3a1a3decb57924989d6b44d81ffbab84d43b95df
RuiDTLima <rui_lima24@hotmail.com>
2017-11-18 11:45:27 +0000

    Correct error made

--- 1º Trabalho/SQL/alineaJ.sql
+++ 1º Trabalho/SQL/alineaJ.sql
@@ -155,7 +155,65 @@ CREATE PROCEDURE dbo.finishEstadaWithFactura @idEstada INT AS
 
 /*************************************** Teste ************************************************************************/
 
+INSERT INTO dbo.ParqueCampismo(nome, morada, estrelas, email)
+	VALUES('Glampinho', 'campo dos parques', 3, 'parque1@email.com')
+	
+INSERT INTO dbo.Hóspede(NIF, nome, morada, email, númeroIdentificação)
+	VALUES(112233445, 'Teste', 'Rua teste', 'teste@teste.com', 11223344),
+	      (566778899, 'Maria', 'Rua 2', 'maria@gmail.com', 55667788),
+		  (123456789, 'Manel', 'Rua Manel', 'manel@test.com', 99001122)
 
+EXEC dbo.InsertAlojamentoTenda 'Glampinho', 'tenda pequena', 'Rua 1', 'bonito', 12, 4, 50
+EXEC dbo.InsertAlojamentoTenda 'Glampinho', 'tenda grande', 'Rua 2', 'grande', 15, 10, 50
+EXEC dbo.InsertAlojamentoTenda 'Glampinho', 'tenda tempo', 'Rua 3', 'tempo', 15, 11, 50
+EXEC dbo.InsertAlojamentoTenda 'Glampinho', 'tenda vazia', 'Rua 4', 'vazia', 15, 10, 50
+EXEC dbo.InsertAlojamentoTenda 'Glampinho', 'tenda nova', 'Rua 5', 'por estrear', 15, 10, 50
+EXEC dbo.InsertAlojamentoBungalow 'Glampinho', 'Bungalow hoje', 'Rua 6', 'primeiro bungalow', 15, 10, 'T3'
+EXEC dbo.InsertAlojamentoBungalow 'Glampinho', 'Bungalow ontem', 'Rua 7', 'segundo bungalow', 15, 9, 'T3'
+
+INSERT INTO dbo.Estada(id, dataInício, dataFim)
+	VALUES (1, '2017-03-15 13:00:00', '2017-03-16 13:00:00'),
+		   (2, '2017-11-12 13:00:00', '2018-11-14 13:00:00'),
+		   (3, '2017-10-05 10:00:00', '2017-11-12 13:00:00'),
+		   (4, '2017-09-12 10:00:00', '2017-09-13 13:00:00'),
+		   (5, '2017-08-10 10:00:00', '2017-09-11 10:00:00')
+
+INSERT INTO dbo.AlojamentoEstada(nomeParque, localização, id, preçoBase)
+	VALUES ('Glampinho', 'Rua 1', 1, 12),
+		   ('Glampinho', 'Rua 2', 1, 15),
+		   ('Glampinho', 'Rua 4', 3, 15),
+		   ('Glampinho', 'Rua 7', 1, 15)
+
+INSERT INTO dbo.Extra(id, descrição, preçoDia, associado)
+	VALUES (1, 'descricao', 10, 'alojamento'), 
+		   (2, 'teste', 15, 'pessoa'), 
+		   (3, 'metodo', 20, 'alojamento')
+
+INSERT INTO dbo.AlojamentoExtra(nomeParque, localização, id)
+	VALUES ('Glampinho', 'Rua 4', 1),
+		   ('Glampinho', 'Rua 4', 2),
+		   ('Glampinho', 'Rua 4', 3)
+
+INSERT INTO dbo.EstadaExtra(estadaId, extraId, preçoDia)
+	VALUES (3, 1, 10),
+		   (3, 2, 15),
+		   (3, 3, 20)
+
+INSERT INTO dbo.HóspedeEstada(NIF, id, hóspede)
+	VALUES (112233445, 3, 'true'),
+		   (566778899, 3, 'false'),
+		   (123456789, 1, 'true')
+
+INSERT INTO dbo.Actividades(nomeParque, númeroSequencial, ano, nome, descrição, lotaçãoMáxima, preçoParticipante, dataRealização)
+	VALUES ('Glampinho', 1, 2017, 'FUT7', 'Jogo de futebol 7vs7', '14', 3, '2017-03-15 10:30:00'),
+		   ('Glampinho', 2, 2017, 'FUT5', 'Jogo de futebol 5vs5', '10', 2, '2017-10-07 10:00:00')
+
+INSERT INTO dbo.Paga(nomeParque, númeroSequencial, ano, NIF, preçoParticipante)
+	VALUES ('Glampinho', 1, 2017, 112233445, 3),
+		   ('Glampinho', 1, 2017, 566778899, 3),
+		   ('Glampinho', 1, 2017, 123456789, 3),
+		   ('Glampinho', 2, 2017, 112233445, 2),
+		   ('Glampinho', 2, 2017, 566778899, 2)
 
 SELECT * FROM dbo.Hóspede
 SELECT * FROM dbo.Alojamento
@@ -179,10 +237,4 @@ EXEC dbo.getPessoalExtrasPreço 3, 1, 2017, @novaLinha, @novaLinha OUTPUT
 EXEc dbo.getCustoTotalActividades 3, 1, 2017, @novaLinha
 
 /* Corre versão final da criação da factura */
-EXEC dbo.finishEstadaWithFactura 5
-EXEC dbo.finishEstadaWithFactura 4
 EXEC dbo.finishEstadaWithFactura 3
-EXEC dbo.finishEstadaWithFactura 2
-EXEC dbo.finishEstadaWithFactura 1
-
-
